6dfaed3e602013ef0525742da3e5b2865b48da36
DOCA2000 <dreamoverseascareerarchitects@gmail.com>
2025-12-13 20:39:04 +0530

    Refactor script.js to manage payment modal
    Removed counter functionality and added payment modal functions.

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-const counters = document.querySelectorAll('.count');
-
-counters.forEach(counter => {
-  const update = () => {
-    const target = +counter.getAttribute('data-target');
-    const current = +counter.innerText;
-    const increment = target / 200;
-
-    if (current < target) {
-      counter.innerText = Math.ceil(current + increment);
-      setTimeout(update, 20);
-    } else {
-      counter.innerText = target;
-    }
-  };
-  update();
-});
+function openPayment(){
+  document.getElementById("paymentModal").style.display="flex";
+}
+function closePayment(){
+  document.getElementById("paymentModal").style.display="none";
+}
